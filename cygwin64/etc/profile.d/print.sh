afa4b638365b3c7fe0c2ad4cefadf4928660a251
Todd Trimmer <todd.trimmer@gmail.com>
2014-04-01 00:47:41 -0500

    better putty CLI for Unix. Perl regex grep. super keygen. networking

--- cygwin64/etc/profile.d/print.sh
+++ cygwin64/etc/profile.d/print.sh
@@ -6,40 +6,16 @@
 
 function printfailure()
 {
-  local banner="FAILED"
-  while getopts "b:" opt; do
-    case "$opt" in
-      b) banner="$OPTARG";;
-    esac
-  done
-  shift $(( $OPTIND - 1 ))
-  OPTIND=0
-  echo -e "[\e[1;31m$banner\e[00m] $@" >&2
+    echo -e "[\e[1;31m$1\e[00m]"
 }
 
 function printsuccess()
 {
-  local banner="SUCCESS"
-  while getopts "b:" opt; do
-    case "$opt" in
-      b) banner="$OPTARG";;
-    esac
-  done
-  shift $(( $OPTIND - 1 ))
-  OPTIND=0
-  echo -e "[\e[1;32m$banner\e[00m] $@" >&2
+    echo -e "[\e[1;32m$1\e[00m]"
 }
 
 function printwarning()
 {
-  local banner="WARNING"
-  while getopts "b:" opt; do
-    case "$opt" in
-      b) banner="$OPTARG";;
-    esac
-  done
-  shift $(( $OPTIND - 1 ))
-  OPTIND=0
-  echo -e "[\e[1;33m$banner\e[00m] $@" >&2
+    echo -e "[\e[1;33m$1\e[00m]"
 }
 
