c34d13e0471500dd4c9c8b9f360656752d59b52a
Todd Trimmer <todd.trimmer@gmail.com>
2016-02-01 16:39:13 -0600

    prevent run on OSX

--- fedora/install.sh
+++ fedora/install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ "$(uname)" == "Darwin" ]; then
+  echo "Aborting! OSX" >&2
+  exit 1
+fi
 SRCROOT=$(dirname "$0")
 ! [ -e $HOME/bin ] && mkdir $HOME/bin
 cp $SRCROOT/home/ttrimmer/.bash* $HOME/
