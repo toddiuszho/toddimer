561d23763b0adffd61bf95ce1f38410714ba1607
Todd Trimmer <todd.trimmer@gmail.com>
2019-01-08 21:10:28 -0600

    Java SSL/TLS tester

---
+++ java/jce-test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget https://github.com/UniconLabs/java-keystore-ssl-test/releases/download/0.0.1/java-keystore-test-0.0.1.jar
+java -jar ./java-keystore-test-0.0.1.jar https://example.com
