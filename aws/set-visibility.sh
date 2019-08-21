4a30e28872d47332d5897f7494e39de600264979
Todd Trimmer <todd.trimmer@gmail.com>
2019-08-21 16:30:06 -0500

    Create set-visibility.sh

---
+++ aws/set-visibility.sh
@@ -0,0 +1,18 @@
+# Set visibility timeout for all queues in AWS Profile whose URL matches the needle
+#
+# Requirements:
+#   aws CLI
+#   awless
+#   AWS profile setup
+#   jq
+#
+
+TIMEOUT_IN_SECONDS=3600
+AWS_PROFILE=my_profile
+NEEDLE=alpha
+
+for queue in $(awless -p $AWS_PROFILE list queues --format json | jq -Mr '.[] | .ID' | grep ${NEEDLE}); do
+  aws --profile $AWS_PROFILE sqs set-queue-attributes \
+    --queue-url "${queue}" \
+    --attributes VisibilityTimeout=${TIMEOUT_IN_SECONDS}
+done
